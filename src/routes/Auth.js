5710ff607e89eca0694307e3c330eab7ad63d5ac
Yunseonghwan <sunghwan0208@naver.com>
2020-09-20 17:06:47 +0900

    Login

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,26 +1,48 @@
 import React, { useState } from "react";
+import { authService } from "../fbase";
 
 const Auth = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [newAccount, setNewAccount] = useState(true);
+  const [error, setError] = useState("");
 
   const onChange = (e) => {
-    const {target: {name, value}} = e;
-    if(name === 'email'){
-        setEmail(value);
-    }else if(name === "password")
-    setPassword(value)
+    const {
+      target: { name, value },
+    } = e;
+    if (name === "email") {
+      setEmail(value);
+    } else if (name === "password") setPassword(value);
   };
 
-  const onSubmit = (e) => {
-      e.preventDefault()
-  }
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    let data;
+    try {
+      if (newAccount) {
+        //create account
+        data = await authService.createUserWithEmailAndPassword(
+          email,
+          password
+        );
+      } else {
+        //log in
+        data = await authService.signInWithEmailAndPassword(email, password);
+      }
+      console.log(data);
+    } catch (error) {
+      setError(error.message);
+    }
+  };
+
+  const toggleAccount = () => setNewAccount((prev) => !prev);
   return (
     <div>
       <form onSubmit={onSubmit}>
         <input
           name="email"
-          type="text"
+          type="email"
           placeholder="Email"
           required
           value={email}
@@ -34,8 +56,10 @@ const Auth = () => {
           value={password}
           onChange={onChange}
         />
-        <input type="submit" value="Log In" />
+        <input type="submit" value={newAccount ? "Create Account" : "Sign In"} />
+        {error}
       </form>
+      <span onClick={toggleAccount}>{newAccount ? "Login" : "Create Account"}</span>
       <div>
         <button>Continue with Google</button>
         <button>Continue with GitHub</button>
